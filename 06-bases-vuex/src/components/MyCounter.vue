c158a73410e1ccc8aa249dda664f2fbf093b0ce1
Axel Montes de Oca <axelmontes@hotmail.com>
2022-04-28 12:03:04 -0500

    06-bases-vuex

---
+++ 06-bases-vuex/src/components/MyCounter.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Counter - Vuex</h1>
+  <!-- <h2>Direct Access: {{ $store.state.count }}</h2> -->
+  <h2>Direct Access: {{ $store.state.counter.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
+
+  <button @click="increment">+1</button>
+  <button @click="incrementBy">+5</button>
+  <button @click="incrementRandomInt" :disabled="isLoading">Random</button>
+
+  <hr>
+
+  <h1>mapState</h1>
+  <h2>mapState: {{ count }}</h2>
+  <h2>lastMutation {{ lastMutation }}</h2>
+
+  <hr>
+
+  <h1>Direct getter</h1>
+  <!-- <h2>Direct getter: {{ $store.getters.squaredCount }}</h2> -->
+  <h2>Direct getter: {{ $store.getters['counter/squaredCount'] }}</h2>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  // computed: mapState(['count'])
+  computed: {
+    countComputed() {
+      return this.$store.state.counter.count
+    },
+    ...mapState('counter', ['count', 'lastMutation', 'isLoading'])
+    // ...mapState({
+      //   count: state => state.count,
+    //   lastMutation: state => state.lastMutation
+    // })
+
+    // ...mapState({
+      //   count: state => state.counter.count,
+    //   lastMutation: state => state.counter.lastMutation
+    // })
+  },
+  methods: {
+    // ...mapActions(['incrementRandomInt']),
+    ...mapActions('counter', ['incrementRandomInt']),
+    increment() {
+      // this.$store.commit('increment')
+      this.$store.commit('counter/increment')
+    },
+    incrementBy() {
+      this.$store.commit('counter/incrementBy', 5)
+    }
+    // incrementRandom() {
+    //   this.$store.dispatch('incrementRandomInt')
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
